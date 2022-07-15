3ae774a304bf61372d7a2a707efb1d4e3a2a68fd
namu6747 <namu6747@naver.com>
2022-07-16 00:32:35 +0900

    2022-07-16 T00:32:00

--- src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
+++ src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThymeleafBasicApplication {
 
-    public static void main(String[] args) {
-
-        SpringApplication.run(
-                ThymeleafBasicApplication.class,
-                args);
-    }
+    public static void main(String[] args) {SpringApplication.run( ThymeleafBasicApplication.class, args);}
 
 }
